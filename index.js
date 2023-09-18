64913725c31d0ec014be4b9cc422f3a10488a4d1
alejandroq12 <jaquezada@innovacion.gob.sv>
2023-09-18 15:34:53 -0600

    Add condition to check first name

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ let prompt = createPrompt();
 let employee = {};
 
 let firstName = prompt('First name: ');
+
+if(!firstName) {
+    console.error(`Invalid first name`);
+    ProcessingInstruction.exit(1);
+}
