3136c5d09ee25e7e586ee9507151147806bae81a
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-09-23 15:24:23 -0600

    Indent code properly

--- while-loops.js
+++ while-loops.js
@@ -8,10 +8,9 @@ while (employees[i]) {
 
 console.log('-----');
 
-
 // Do while loop
 i = 0;
 do {
-    console.log(`Name: ${employees[i].firstName} ${employees[i].lastName}`)
-    i++;
-} while(employees[i]);
+  console.log(`Name: ${employees[i].firstName} ${employees[i].lastName}`);
+  i++;
+} while (employees[i]);
