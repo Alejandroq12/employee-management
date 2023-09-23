d6f18c15f1d51ce3144ed6f1b0160bed7c8045a9
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-09-23 15:33:55 -0600

    Add for loop

--- for-loops.js
+++ for-loops.js
@@ -1,2 +1,6 @@
 // Array of Data
-const namnes = ['Julio', 'Alejandro', 'Roberto', 'Artero'];
+const names = ['Julio', 'Alejandro', 'Roberto', 'Artero'];
+
+for (let i = 0; i < names.length; i++) {
+  console.log(`Name: ${names[i]}`);
+}
