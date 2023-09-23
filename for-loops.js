c84775b62ec5e8ab976ede047cbb024802d5e51a
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-09-23 15:37:00 -0600

    Add for of loop

--- for-loops.js
+++ for-loops.js
@@ -4,3 +4,10 @@ const names = ['Julio', 'Alejandro', 'Roberto', 'Artero'];
 for (let i = 0; i < names.length; i++) {
   console.log(`Name: ${names[i]}`);
 }
+
+console.log('-----');
+
+// Using a for...of loop
+for (let name of names) {
+  console.log(`Names: ${name}`);
+}
