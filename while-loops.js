0615b231aa2715cdf1785d019453a2af08445737
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-09-23 14:26:10 -0600

    Iterate over the employees array

--- while-loops.js
+++ while-loops.js
@@ -1,3 +1,7 @@
 import employees from './data.json' assert { type: 'json'}
 
 let i = 0;
+while(employess[i]) {
+    console.log(`Name: ${employess[i].firstName} ${employess[i].lastName}`);
+    i++;
+}
