08acc6548446b05df4362a3f7d600c54b30f96f1
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-09-23 22:18:51 -0600

    Searching only for the first names that start with 'B'

--- loop-flow.js
+++ loop-flow.js
@@ -1,1 +1,20 @@
-import employees from './data.json' assert { type: 'json' }
+import employees from './data.json' assert { type: 'json' };
+
+// Listing only the first five items
+for (let i = 0; i < employees.length; i++) {
+  if (i === 5) {
+    break;
+  }
+  console.log(`Name: ${employees[i].firstName} ${employees[i].lastName}`);
+}
+
+console.log('-----');
+
+// Searching only for the first names that start with 'B'.
+for (let employee of employees) {
+  if (!employee.firstName.startsWith('B')) {
+    // Continue means that we will stop the current iteration but we will continue to the next iteration
+    continue;
+  }
+  console.log(`Name: ${employee.firstName} ${employee.lastName}`);
+}
