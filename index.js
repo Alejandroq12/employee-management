04f3173c132d8607fcc135ac1986b1ac63efab28
alejandroq12 <jaquezada@innovacion.gob.sv>
2023-09-25 10:20:19 -0600

    Refactor code to make it more reusable

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ let prompt = createPrompt();
 
 function getInput(promptText, validator, transformer) {
   let value = prompt(promptText);
+  if (validator && !validator(value)) {
+    console.error(`--Invalid input`);
+    process.exit(1);
+  }
   return value;
 }
 // Validator functions -------------------------
@@ -53,79 +57,27 @@ function listEmployees() {
 function addEmployee() {
   console.log('Add employee ---------------------');
   console.log('');
-
   let employee = {};
-
-  let firstName = prompt('First name: ');
-
-  if (!firstName) {
-    console.error(`Invalid first name`);
-    process.exit(1);
-  }
-  employee.firstName = firstName;
-
-  let lastName = prompt('Last name: ');
-  if (!lastName) {
-    console.error(`Invalid last name`);
-    process.exit(1);
-  }
-
-  employee.lastName = lastName;
-
-  let startDateYear = prompt('Employee Start Year (1990-2023): ');
-  startDateYear = Number(startDateYear);
-  // Check if it is a valid integer
-  if (!Number.isInteger(startDateYear)) {
-    console.error(`Enter a valid start date year`);
-    process.exit(1);
-  }
-  // Check if the number is in the range
-  if (startDateYear < 1990 || startDateYear > 2023) {
-    console.error(`Enter a start date year within the correct range`);
-    process.exit(1);
-  }
-
-  let startDateMonth = prompt('Employee Start Date Month (1-12): ');
-  startDateMonth = Number(startDateMonth);
-  // Check if it is a valid integer
-  if (!Number.isInteger(startDateMonth)) {
-    console.error(`Enter a valid start date month`);
-    process.exit(1);
-  }
-  // Check if the number is in the range
-  if (startDateMonth < 1 || startDateMonth > 12) {
-    console.error(`Enter a start date month within the correct range`);
-    process.exit(1);
-  }
-
-  let startDateDay = prompt('Employee start date day (1-31): ');
-  startDateDay = Number(startDateDay);
-  // Check if it is a valid integer
-  if (!Number.isInteger(startDateDay)) {
-    console.error(`Enter a valid start date day`);
-    process.exit(1);
-  }
-  // Check if the number is in the range
-  if (startDateDay < 1 || startDateDay > 31) {
-    console.error(`Enter a start date day within the correct range`);
-    process.exit(1);
-  }
-
-  // Date elements are correct, lets create the table
+  employee.firstName = getInput('First name: ', isStringInputValid);
+  employee.lastName = getInput('Last name: ', isStringInputValid);
+  let startDateYear = getInput(
+    'Start date year (1990-2023): ',
+    isStartYearValid
+  );
+  let startDateMonth = getInput('Start date month (1-12): ', isStartMonthValid);
+  let startDateDay = getInput(
+    'Employee start date day (1-31): ',
+    isStartDayValid
+  );
   employee.startDate = new Date(
     startDateYear,
     startDateMonth - 1,
     startDateDay
   );
-
-  let isActive = prompt('Is employee active (yes or no): ');
-  // Check if incorrect input
-  if (isActive !== 'yes' && isActive !== 'no') {
-    console.error(`Enter yes or no for employee active status`);
-    process.exit(1);
-  }
-
-  employee.isActive = isActive === 'yes';
+  employee.isActive = getInput(
+    'Is employee active (yes or no): ',
+    isBooleanInputValid
+  );
 
   // Output Employee JSON
   const json = JSON.stringify(employee, null, 2);
