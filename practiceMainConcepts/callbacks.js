e9bb2550d42749220e7649c1b86d869d9141f989
alejandroq12 <jaquezada@innovacion.gob.sv>
2023-09-26 14:44:27 -0600

    Add callbacks exercise

---
+++ practiceMainConcepts/callbacks.js
@@ -0,0 +1,16 @@
+import fs from 'node:fs';
+
+fs.readFile('./data2.json', 'utf8', (err, data) => {
+  if (err) {
+    console.log(`Error reading the file`);
+    throw err;
+  }
+  try {
+  const dataObj = JSON.parse(data);
+  console.log(dataObj);
+  console.log('Complete');
+  } catch(err) {
+    console.error('Cannot parse JSON from file');
+    throw err;
+  }
+});
