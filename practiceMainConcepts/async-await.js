04283a7a6d49d31719e2a5b6e7804ad562bab6ee
alejandroq12 <jaquezada@innovacion.gob.sv>
2023-09-28 09:36:21 -0600

    Add async-await practice

--- practiceMainConcepts/async-await.js
+++ practiceMainConcepts/async-await.js
@@ -1,12 +1,35 @@
 import fs from 'node:fs/promises';
 
-async function loadData() {
-  try {
-    const data = await fs.readFile('./data2.json', 'utf8');
-    const dataObj = JSON.parse(data);
-    console.log(dataObj);
-    console.log('Complete');
-  } catch {}
-}
+// async function loadData() {
+//   try {
+//     const data = await fs.readFile('./data2.json', 'utf8');
+//     console.log('File read 1');
+//     await fs.readFile('./data2.json', 'utf8');
+//     console.log('File read 2');
+//     await fs.readFile('./data2.json', 'utf8');
+//     console.log('File read 3');
+//     const dataObj = JSON.parse(data);
+//     console.log(dataObj);
+//     console.log('Complete');
+//   } catch(error) {
+//     console.log('Could not load and parse file');
+//     throw error;
+//   }
+// }
 
-loadData().then(() => console.log('Promise completed'));
+// loadData().then(() => console.log('Promise completed'));
+
+try {
+  const data = await fs.readFile('./data2.json', 'utf8');
+  console.log('File read 1');
+  await fs.readFile('./data2.json', 'utf8');
+  console.log('File read 2');
+  await fs.readFile('./data2.json', 'utf8');
+  console.log('File read 3');
+  const dataObj = JSON.parse(data);
+  console.log(dataObj);
+  console.log('Complete');
+} catch(error) {
+  console.log('Could not load and parse file');
+  throw error;
+}
