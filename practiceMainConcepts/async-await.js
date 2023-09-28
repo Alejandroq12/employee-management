3e41532eb4fd61c6b88bfecf325bee365759cd34
alejandroq12 <jaquezada@innovacion.gob.sv>
2023-09-28 09:18:54 -0600

    try catch block

--- practiceMainConcepts/async-await.js
+++ practiceMainConcepts/async-await.js
@@ -1,5 +1,12 @@
-import fs from "node:fs/promises";
+import fs from 'node:fs/promises';
 
 async function loadData() {
-    
+  try {
+    const data = await fs.readFile('./data2.json', 'utf8');
+    const dataObj = JSON.parse(data);
+    console.log(dataObj);
+    console.log('Complete');
+  } catch {}
 }
+
+loadData().then(() => console.log('Promise completed'));
