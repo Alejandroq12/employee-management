c27f444a9f31e36a2648dca790e70f7165f85945
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-09-28 20:23:59 -0600

    Fix(indentation): fix code indentation.

--- practiceMainConcepts/fetch.js
+++ practiceMainConcepts/fetch.js
@@ -1,17 +1,20 @@
 const myHeaders = new Headers();
-myHeaders.append("apikey", "f463d8dd3c1494aae0aeca639bb18eee");
+myHeaders.append('apikey', 'f463d8dd3c1494aae0aeca639bb18eee');
 
 const requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-}
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow',
+};
 
 try {
-    const result = await fetch("https://api.apilayer.com/exchangerates_data/latest?base=USD", requestOptions)
-    const resultObj = await result.json();
-    console.log(JSON.stringify(resultObj, null, 2))
-} catch(err) {
-    console.error(`Could not fetch currency data`)
-    throw err;
+  const result = await fetch(
+    'https://api.apilayer.com/exchangerates_data/latest?base=USD',
+    requestOptions
+  );
+  const resultObj = await result.json();
+  console.log(JSON.stringify(resultObj, null, 2));
+} catch (err) {
+  console.error(`Could not fetch currency data`);
+  throw err;
 }
