dd0a2595396f87d82f95c6e2d93f9577400890e9
alejandroq12 <jaquezada@innovacion.gob.sv>
2023-10-02 13:55:48 -0600

    Fix(eslint): Linter errors

--- currency.js
+++ currency.js
@@ -1,24 +1,24 @@
-let access_key = "f463d8dd3c1494aae0aeca639bb18eee";
+const access_key = 'f463d8dd3c1494aae0aeca639bb18eee';
 
 // Currency data ----------------------------------------
 export const getCurrencyConversionData = async () => {
-    var requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    const response = await fetch(`http://api.exchangeratesapi.io/v1/latest?access_key=${access_key}&base=EUR`, requestOptions)
-    if(!response.ok) {
-      throw new Error("Cannot fetch currency data");
-    }
-    return await response.json();
+  const requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+  };
+
+  const response = await fetch(`http://api.exchangeratesapi.io/v1/latest?access_key=${access_key}&base=EUR`, requestOptions);
+  if (!response.ok) {
+    throw new Error('Cannot fetch currency data');
   }
-  
-  export const getSalary = (amountEUR, currency, currencyData) => {
-    const amount = (currency === "EUR") ? amountEUR : amountEUR * currencyData.rates[currency];
-    const formatter = Intl.NumberFormat('de-DE', {
-      style: 'currency',
-      currency: currency
-    });
-    return formatter.format(amount);
-  }
+  return response.json();
+};
+
+export const getSalary = (amountEUR, currency, currencyData) => {
+  const amount = (currency === 'EUR') ? amountEUR : amountEUR * currencyData.rates[currency];
+  const formatter = Intl.NumberFormat('de-DE', {
+    style: 'currency',
+    currency,
+  });
+  return formatter.format(amount);
+};
